586c916a928b2af8eaebcf3a7d64deb51bf585c4
Niko Savola <niko.savola@aalto.fi>
2025-08-29 18:14:03 +0300

    Add a launcher component to PDK

--- qpdk/cells/__init__.py
+++ qpdk/cells/__init__.py
@@ -3,6 +3,7 @@
 from qpdk.cells.coupler_capacitive import *
 from qpdk.cells.flux_qubit import *
 from qpdk.cells.interdigital_capacitor import *
+from qpdk.cells.launcher import *
 from qpdk.cells.resonator import *
 from qpdk.cells.transmon import *
 from qpdk.cells.waveguides import *
