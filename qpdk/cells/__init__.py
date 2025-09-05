735783ba88e4e2f0e96b5d144cb9e208ae3a53e2
Niko Savola <niko.savola@aalto.fi>
2025-09-05 18:10:48 +0300

    Add circle to PDK

--- qpdk/cells/__init__.py
+++ qpdk/cells/__init__.py
@@ -1,5 +1,7 @@
 """Quantum PDK cells."""
 
+import gdsfactory as gf
+
 from qpdk.cells.bump import *
 from qpdk.cells.capacitor import *
 from qpdk.cells.junction import *
@@ -7,3 +9,5 @@ from qpdk.cells.launcher import *
 from qpdk.cells.resonator import *
 from qpdk.cells.transmon import *
 from qpdk.cells.waveguides import *
+
+circle = gf.components.circle
