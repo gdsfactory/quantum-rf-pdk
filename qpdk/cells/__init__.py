fbdb9f23db38c9a58551c1776ccb18dd74882c89
Niko Savola <niko.savola@aalto.fi>
2025-09-05 17:33:07 +0300

    Remove false flux qubits

--- qpdk/cells/__init__.py
+++ qpdk/cells/__init__.py
@@ -1,7 +1,6 @@
 """Quantum PDK cells."""
 
 from qpdk.cells.capacitor import *
-from qpdk.cells.flux_qubit import *
 from qpdk.cells.junction import *
 from qpdk.cells.launcher import *
 from qpdk.cells.resonator import *
