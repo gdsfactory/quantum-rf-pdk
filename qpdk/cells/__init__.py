d7fd4a7a44431b3999fcf2a3140c3795c8b7090b
Niko Savola <niko.savola@aalto.fi>
2025-09-05 15:53:29 +0300

    Merge capacitors to one file

--- qpdk/cells/__init__.py
+++ qpdk/cells/__init__.py
@@ -1,8 +1,8 @@
 """Quantum PDK cells."""
 
-from qpdk.cells.coupler_capacitive import *
+from qpdk.cells.capacitor import *
 from qpdk.cells.flux_qubit import *
-from qpdk.cells.interdigital_capacitor import *
+from qpdk.cells.helpers import *
 from qpdk.cells.junction import *
 from qpdk.cells.launcher import *
 from qpdk.cells.resonator import *
