7b3547d401a5c2d6a56fd48b9aa78ebc5383ef3c
Niko Savola <niko.savola@aalto.fi>
2025-09-08 11:57:59 +0300

    Revert adding transform_component helper to __init__

--- qpdk/cells/__init__.py
+++ qpdk/cells/__init__.py
@@ -4,7 +4,6 @@ import gdsfactory as gf
 
 from qpdk.cells.bump import *
 from qpdk.cells.capacitor import *
-from qpdk.cells.helpers import transform_component  # Import only the non-cell helpers
 from qpdk.cells.junction import *
 from qpdk.cells.launcher import *
 from qpdk.cells.resonator import *
