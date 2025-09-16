d92c665f279cfca13debb3dc0996e8e677dd5959
Niko Savola <niko.savola@aalto.fi>
2025-09-16 09:31:12 +0300

    Fix sidewall angles in tech.py

--- qpdk/tech.py
+++ qpdk/tech.py
@@ -112,7 +112,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=200e-9 * 1e6,
                 zmin=0.0,  # top of substrate
                 material="Nb",
-                sidewall_angle=90.0,
                 mesh_order=1,
             ),
             "Substrate": LayerLevel(
@@ -121,7 +120,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=500,  # 500 microns of silicon
                 zmin=-500,  # below metal
                 material="Si",
-                sidewall_angle=90.0,
                 mesh_order=4,
             ),
             "Vacuum": LayerLevel(
@@ -130,7 +128,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=500e-6 * 1e6,  # 500 microns of vacuum above metal
                 zmin=200e-9 * 1e6,  # above metal
                 material="vacuum",
-                sidewall_angle=90.0,
                 mesh_order=99,
             ),
             # Airbridge metal sitting above M1 (example: +300 nm)
@@ -140,7 +137,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=200e-9 * 1e6,
                 zmin=300e-9 * 1e6,  # stacked above via
                 material="Nb",
-                sidewall_angle=90.0,
             ),
             "Airbridge_Via": LayerLevel(
                 name="Airbridge_Via",
@@ -148,7 +144,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=100e-9 * 1e6,
                 zmin=200e-9 * 1e6,  # stacked above M1
                 material="Nb",
-                sidewall_angle=90.0,
             ),
             # JJ_AREA can be exported as a thin film if you use it in EM
             "JosephsonJunction": LayerLevel(
@@ -157,7 +152,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=70e-9,
                 zmin=0,
                 material="AlOx/Al",
-                sidewall_angle=90.0,
                 mesh_order=2,
             ),
             "TSV": LayerLevel(
@@ -166,7 +160,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=500,  # full substrate thickness
                 zmin=-500,  # starting at bottom
                 material="TiN",
-                sidewall_angle=90.0,
                 mesh_order=3,
             ),
             "IndiumBump": LayerLevel(
@@ -175,7 +168,6 @@ def get_layer_stack() -> LayerStack:
                 thickness=10,  # 10 microns
                 zmin=200e-9 * 1e6,  # stacked above M1
                 material="In",
-                sidewall_angle=90.0,
                 mesh_order=3,
             ),
         }
@@ -203,7 +195,6 @@ LAYER_STACK_FLIP_CHIP = LayerStack(
             thickness=0.2,
             zmin=10.0,
             material="Nb",
-            sidewall_angle=90.0,
             mesh_order=1,
         ),
         "Substrate_top": LayerLevel(
@@ -212,7 +203,6 @@ LAYER_STACK_FLIP_CHIP = LayerStack(
             thickness=500,  # 500 microns of silicon
             zmin=10.2,  # below metal
             material="Si",
-            sidewall_angle=90.0,
             mesh_order=4,
         ),
     }
