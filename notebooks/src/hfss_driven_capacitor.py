Fix HFSS frequency scaling and cast S-parameters to complex128

notebooks/src/hfss_driven_capacitor.py
@@ -332,8 +332,8 @@ fig, axes = plt.subplots(2, 1, figsize=(10, 8))
 s11_col = next(col for col in df_results.columns if "S(o1:1,o1:1)" in col)
 s21_col = next(col for col in df_results.columns if "S(o2:1,o1:1)" in col)
 
-s11_trace = df_results[s11_col].to_numpy()
-s21_trace = df_results[s21_col].to_numpy()
+s11_trace = df_results[s11_col].to_numpy().astype(np.complex128)
+s21_trace = df_results[s21_col].to_numpy().astype(np.complex128)
 
 s11_mag_db = 20 * np.log10(np.abs(s11_trace))
 s21_mag_db = 20 * np.log10(np.abs(s21_trace))

qpdk/models/hfss.py
@@ -575,7 +575,7 @@ def get_sparameter_results(
         )
         if solution:
             if "frequency_ghz" not in data:
-                data["frequency_ghz"] = np.array(solution.primary_sweep_values) / 1e9
+                data["frequency_ghz"] = np.array(solution.primary_sweep_values)
 
             # Get complex S-parameter data
             # Use get_expression_data to get real and imaginary parts